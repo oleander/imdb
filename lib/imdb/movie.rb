2620179ad756690be315dfc3485ef929c95ab311
Linus Oleander <linus@oleander.nu>
2010-08-08 03:51:15 +0200

    Fix some bugs

--- lib/imdb/movie.rb
+++ lib/imdb/movie.rb
@@ -74,7 +74,7 @@ module Imdb
     end
     
     def votes
-      document.at("#tn15rating .tn15more").gsub(/[^\d+]/, "").to_i rescue nil
+      document.at("a.tn15more").gsub(/[^\d+]/, "").to_i rescue nil
     end
     
     # Returns a string containing the tagline
